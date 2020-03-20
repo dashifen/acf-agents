dfbf660cd35f9291ce7a06115f0776cee12918be
David Dashifen Kees <dashifen@dashifen.com>
2020-03-20 17:00:55 -0400

    initial commit

---
+++ src/Repositories/FieldGroup.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Dashifen\ACFAgent\Repositories;
+
+use Dashifen\Repository\Repository;
+
+/**
+ * Class FieldGroup
+ *
+ * @property-read array $content
+ * @property-read int   $lastModified
+ *
+ * @package Dashifen\ACFAgent\Repositories
+ */
+class FieldGroup extends Repository
+{
+    /**
+     * @var array
+     */
+    protected $content;
+    
+    /**
+     * @var int
+     */
+    protected $lastModified = 0;
+    
+    /**
+     * setContent
+     *
+     * Sets the content property, and if the title is empty, sets that one,
+     * too.
+     *
+     * @param array $content
+     *
+     * @return void
+     */
+    protected function setContent (array $content): void
+    {
+        $this->content = $content;
+    }
+    
+    /**
+     * setLastModified
+     *
+     * Sets the last modified property.
+     *
+     * @param int $lastModified
+     *
+     * @return void
+     */
+    protected function setLastModified (int $lastModified): void
+    {
+        $this->lastModified = $lastModified;
+    }
+}
